80c116521eebbebb0fed6f976bfd01eed70ea8c0
Maximilian Remming <maxremming@gmail.com>
2017-05-25 16:04:54 +0300

    Moved code in a single module

--- ppgr/ppgr.py
+++ ppgr/ppgr.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-
-import argparse
 import time
 import sys
 
 from contextlib import contextmanager
 from collections import namedtuple
 
-import screen
+from .screen import Screen, terminal_size
+
 
 Point = namedtuple("Point", ("x", "y"))
 
@@ -47,7 +45,7 @@ class PPGR:
         self.limit = limit
 
         self._ps = []
-        self._canvas = screen.Screen(to_int=round)
+        self._canvas = Screen(to_int=round)
         self._t = 0
 
         self._max_x = None
@@ -66,7 +64,7 @@ class PPGR:
         if min_y is None:
             min_y = self._min_y
 
-        w, h = screen.terminal_size()
+        w, h = terminal_size()
 
         try:
             x_fact = w / (max_x - min_x)
@@ -159,75 +157,3 @@ class PPGR:
             str(self._canvas),
             wait=None if no_animate else self.wait,
             end="\n")
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="terminal graphing tool that supports piped data with realtime updates")
-
-    parser.add_argument(
-        "-f", "--format",
-        nargs="+",
-        default=["a"],
-        choices=("a", "t", "d", "s"))
-    parser.add_argument(
-        "-x", "--fail-bad-line",
-        action="store_true",
-        help="fail if input line can't be parsed")
-    parser.add_argument(
-        "-w", "--wait",
-        type=int,
-        metavar="N",
-        help="wait at least N milliseconds between frames")
-    parser.add_argument(
-        "-t", "--time-scale",
-        type=float)
-    parser.add_argument(
-        "-l", "--limit",
-        type=int)
-    parser.add_argument(
-        "-s", "--no-animate",
-        action="store_true")
-    parser.add_argument(
-        "-i", "--input",
-        type=argparse.FileType("r"),
-        default="-")
-
-    parser.add_argument(
-        "--max-x",
-        type=float)
-    parser.add_argument(
-        "--min-x",
-        type=float)
-    parser.add_argument(
-        "--max-y",
-        type=float)
-    parser.add_argument(
-        "--min-y",
-        type=float)
-
-    argv = parser.parse_args()
-    ppgr = PPGR(
-        argv.format,
-        argv.fail_bad_line,
-        argv.wait,
-        argv.time_scale,
-        argv.limit)
-
-    with no_cursor():
-        for line in argv.input:
-            ppgr.line(line)
-            if not argv.no_animate:
-                ppgr.show(argv.max_x, argv.min_x, argv.max_y, argv.min_y)
-        if argv.no_animate:
-            ppgr.show(argv.max_x, argv.min_x, argv.max_y, argv.min_y, True)
-
-
-def __main__():
-    try:
-        main()
-    except KeyboardInterrupt:
-        sys.exit(0)
-
-if __name__ == "__main__":
-    __main__()
