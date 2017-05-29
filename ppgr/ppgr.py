3145bb5e88bb4ec23e092ccbb6f6cd073f8a6f68
Maximilian Remming <maxremming@gmail.com>
2017-05-30 00:37:04 +0300

    Fancy new Points class, and refactoring

--- ppgr/ppgr.py
+++ ppgr/ppgr.py
@@ -1,55 +1,88 @@
-import time
+from time import monotonic
 from collections import namedtuple
 
 from .screen import Screen
 from .terminal import write
 
 Point = namedtuple("Point", ("x", "y"))
+Rectangle = namedtuple("Rectangle", ("x1", "y1", "x2", "y2"))
 
 
-class MinMax:
-    def __init__(self, min_x=None, min_y=None, max_x=None, max_y=None):
-        self.min_x = min_x
-        self.min_y = min_y
-        self.max_x = max_x
-        self.max_y = max_y
+class Points:
+    def __init__(self, limit=None):
+        self.limit = limit
 
-    def update(self, p):
-        if self.min_x is None:
-            self.min_x = p.x
-        if self.min_y is None:
-            self.min_y = p.y
+        self._ps = []
+        self._min_x = None
+        self._min_y = None
+        self._max_x = None
+        self._max_y = None
 
-        if self.max_x is None:
-            self.max_x = p.x
-        if self.max_y is None:
-            self.max_y = p.y
+    def _update(self, p):
+        if self._min_x is None:
+            self._min_x = p.x
+        elif self._min_x > p.x:
+            self._min_x = p.x
 
-        if self.min_x > p.x:
-            self.min_x = p.x
-        if self.min_y > p.y:
-            self.min_y = p.y
+        if self._min_y is None:
+            self._min_y = p.y
+        elif self._min_y > p.y:
+            self._min_y = p.y
 
-        if self.max_x < p.x:
-            self.max_x = p.x
-        if self.max_y < p.y:
-            self.max_y = p.y
+        if self._max_x is None:
+            self._max_x = p.x
+        elif self._max_x < p.x:
+            self._max_x = p.x
 
-    def recalculate(self, ps):
-        self.min_x = None
-        self.min_y = None
-        self.max_x = None
-        self.max_y = None
+        if self._max_y is None:
+            self._max_y = p.y
+        elif self._max_y < p.y:
+            self._max_y = p.y
 
-        for p in ps:
-            self.update(p)
+    def _recalculate_bounds(self):
+        self._min_x = None
+        self._min_y = None
+        self._max_x = None
+        self._max_y = None
+        for p in self._ps:
+            self._update(p)
+
+    def add(self, p):
+        self._ps.append(p)
+
+        if self.limit is not None and len(self._ps) > self.limit:
+            self._ps[:] = self._ps[len(self._ps) - self.limit:]
+            self._recalculate_bounds()
+        else:
+            self._update(p)
+
+    def points(self):
+        for p in self._ps:
+            yield p
+
+    def bounds(self, b=None):
+        if len(self._ps) > 0:
+            assert(4 == len(list(filter(None, (
+                self._min_x, self._min_y,
+                self._max_x, self._max_y)))))
+        elif len(self._ps) == 0:
+            return Rectangle(0, 0, 0, 0)
+
+        if b is None:
+            b = Rectangle(None, None, None, None)
+
+        return Rectangle(
+            self._min_x if b.x1 is None else b.x1,
+            self._min_y if b.y1 is None else b.y1,
+            self._max_x if b.x2 is None else b.x2,
+            self._max_y if b.y2 is None else b.y2)
 
 
 class PPGR:
     def __init__(self, format, fail_bad_line=False, wait=None, time_scale=None, limit=None):
         self.format = format
         self.fail_bad_line = fail_bad_line
-        self.limit = limit
+        self.ps = Points(limit)
 
         self.wait = wait
         if self.wait is not None:
@@ -57,71 +90,40 @@ class PPGR:
 
         self.time_scale = time_scale
         if self.time_scale is None:
-            self.t0 = time.monotonic()
+            self.t0 = monotonic()
 
-        self.ps = []
         self.screen = Screen()
         self.t = 0
-        self.min_max = MinMax()
 
     def _prep_canvas(self, max_x=None, min_x=None, max_y=None, min_y=None):
         """preps the canvas so that it can be drawn"""
 
-        def f(length, ma, mi):
+        def f(l1, l2):
             try:
-                return length / (ma - mi)
-            except (ZeroDivisionError, TypeError):
+                return l1 / l2
+            except ZeroDivisionError:
                 return 1
 
-        def g(p):
+        def scale(p):
+            """Tranform point p from internal coordinates to screen coordinates."""
+
             return Point(
-                (p.x - low.x) * fact.x,
-                size.y - ((p.y - low.y) * fact.y))
-
-        if min_x is None:
-            min_x = self.min_max.min_x
-        if min_y is None:
-            min_y = self.min_max.min_y
-        low = Point(min_x, min_y)
-
-        if max_x is None:
-            max_x = self.min_max.max_x
-        if max_y is None:
-            max_y = self.min_max.max_y
-        high = Point(max_x, max_y)
+                fact.x * (p.x - bounds.x1),
+                size.y - fact.y * (p.y - bounds.y1))
 
         self.screen.size = None, None
         size = Point(*self.screen.size)
-        fact = Point(f(size.x, high.x, low.x), f(size.y, high.y, low.y))
+        bounds = self.ps.bounds(Rectangle(min_x, min_y, max_x, max_y))
+        fact = Point(
+            f(size.x, abs(bounds.x1 - bounds.x2)),
+            f(size.y, abs(bounds.y1 - bounds.y2)))
 
-        for p in map(g, self.ps):
+        for p in map(scale, self.ps.points()):
             self.screen(*p)
-
-    def _max_min(self, many):
-        """updates mins and maxes based on the last `many` points"""
-
-        for i in range(many):
-            last = self.ps[-(many)]
-            self.min_max.update(last)
-
-    def _drop_extra(self):
-        """drops extra points and recalculates mins and maxes if limit is set"""
-
-        if self.limit is None:
-            return
-
-        old_len = len(self.ps)
-
-        # store at most limit points
-        self.ps[:] = self.ps[len(self.ps) - self.limit:]
-
-        # recalculate mins and maxs if the some items were removed
-        if old_len > len(self.ps):
-            self.min_max.recalculate(self.ps)
 
     def _update_t(self):
         if self.time_scale is None:
-            self.t = time.monotonic() - self.t0
+            self.t = monotonic() - self.t0
         else:
             self.t += self.time_scale
 
@@ -153,17 +155,12 @@ class PPGR:
                 if i == "s":
                     a.pop()
                     continue
-                self.ps.append(f[i](a))
+                self.ps.add(f[i](a))
                 many += 1
             except IndexError as e:
                 if self.fail_bad_line:
                     raise Exception("bad line: {} failed".format(_line))
 
-        # keep track of mins and maxs while reading data
-        # it's faster than calculcating mins and maxs everytime
-        self._max_min(many)
-
-        self._drop_extra()
         self._update_t()
 
     def show(self, max_x=None, min_x=None, max_y=None, min_y=None, no_animate=False, newline=False):
