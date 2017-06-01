8a23a1708a77132216477aa9a0413fbca989eddd
Maximilian Remming <maxremming@gmail.com>
2017-06-01 11:28:55 +0300

    Added python versions

--- setup.py
+++ setup.py
@@ -26,6 +26,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Visualization",
-        "Topic :: Utilities"
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ]
 )
